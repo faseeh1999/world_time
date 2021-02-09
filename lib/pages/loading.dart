9c9c0521d6fe187d16bbf5a5267edb6b623c013c
Faseeh PC <faseehshahzad.fs@gmail.com>
2021-02-09 19:07:25 +0500

    Lecture 32
    Code Ninja Flutter Beginner.

--- lib/pages/loading.dart
+++ lib/pages/loading.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:world_time/services/world_time.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 
 class Loading extends StatefulWidget {
   @override
@@ -50,9 +51,14 @@ class _LoadingState extends State<Loading> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Padding(
-        padding: EdgeInsets.all(50.0),
-        child: Text("Loading"),
+      backgroundColor: Colors.blue[900],
+      body: Center(
+
+        child: SpinKitRipple(
+          color: Colors.white,
+          size: 50.0,
+        ),
+
       ),
     );
   }
